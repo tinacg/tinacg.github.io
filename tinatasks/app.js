6df143ac21f5115a7db60613b46dc8f000db9885
Heitor Chang <heitorpontual@gmail.com>
2014-11-09 21:27:43 -0200

    add tabs

--- tinatasks/app.js
+++ tinatasks/app.js
@@ -1,43 +1,69 @@
 (function() {
-  var app = angular.module('tinatasks', ["firebase"]);
+  var app = angular.module('tinatasks', ["components", "firebase"]);
   var ref = new Firebase("https://tinacg.firebaseio.com/tinatasks");
   
-  app.controller('TaskController', ['$scope', '$firebase', function($scope, $firebase) {
+  app.controller('TaskController', ['$rootScope', '$scope', '$firebase', function($rootScope, $scope, $firebase) {
     function init(authData) {
       $scope.loginStatus = "Welcome back " + authData.password.email + "! Your token expires " + (new Date(authData.expires * 1000));
       $scope.loggedIn = true;
 
-      var tasksRef = ref.child(authData.uid);
+      var tabsRef = ref.child(authData.uid).child("tabs");
+      var sync = $firebase(tabsRef);
+      $scope.tabs = sync.$asArray();
       
-      // until new security rules are placed, there is nothing preventing
-      // an user from overwriting another user's data or reading them.
+      $scope.addTab = function(tabName) {
+        $scope.tabs.$add({ name: tabName })
+          .then(function() { $scope.refreshTaskRefs(); });
+      };
 
-      var sync = $firebase(tasksRef);
-      $scope.tasks = sync.$asArray();
+      var tasksRef = [];
+      var tasksSync = [];
+      $scope.tasks = [];
 
-      $scope.addTask = function(taskDone, taskDescription, taskCategory, taskDueDate) {
+      var tasksRootRef = ref.child(authData.uid).child("tasks");
+      var tasksRootSync = $firebase(tasksRootRef);
+      $scope.tasksRoot = tasksRootSync.$asObject();
+      
+      $scope.refreshTaskRefs = function() {
+        angular.forEach($scope.tabs, function(tab, tabId) {
+          tasksRef[tab.name] = ref.child(authData.uid).child("tasks").child(tab.name);
+          tasksSync[tab.name] = $firebase(tasksRef[tab.name]);
+          $scope.tasks[tab.name] = tasksSync[tab.name].$asArray();
+        });
+      };
+
+      $scope.tabs.$loaded().then(function () {
+        $scope.refreshTaskRefs();
+        //$rootScope.panes[1].select();
+      });
+      
+      $scope.addTask = function(tabName, taskDone, taskDescription, taskCategory, taskDueDate) {
         taskDescription = taskDescription || "My Task";
         taskCategory = taskCategory || "~uncategorized";
-        taskDueDate = taskDueDate || "asap";
+        taskDueDate = taskDueDate || "soon";
         
-        $scope.tasks.$add({ done: taskDone,
-                            description: taskDescription,
-                            category: taskCategory,
-                            createDate: (new Date()).getTime(),
-                            dueDate: taskDueDate,
-                          });
+        $scope.tasks[tabName].$add({ done: taskDone,
+                                     description: taskDescription,
+                                     category: taskCategory,
+                                     createDate: (new Date()).getTime(),
+                                     dueDate: taskDueDate,
+                                   });
         $scope.taskDone = false;
         $scope.taskDescription = "";
         $scope.taskCategory = "";
         $scope.taskDueDate = "";
-
+        
         $scope.$broadcast("newTaskAdded");
+      };
+
+      $scope.deleteTabTasks = function(tabName) {
+        tasksRootSync.$remove(tabName);
       };
     }
 
     function clean() {
       $scope.loginStatus = "Please log in";
-      $scope.tasks = [];
+      $scope.tabs = [];
       $scope.loggedIn = false;
     }
 
---
+++ tinatasks/app_single.js
@@ -0,0 +1,64 @@
+(function() {
+  var app = angular.module('tinatasks', ["firebase"]);
+  var ref = new Firebase("https://tinacg.firebaseio.com/tinatasks");
+  
+  app.controller('TaskController', ['$scope', '$firebase', function($scope, $firebase) {
+    function init(authData) {
+      $scope.loginStatus = "Welcome back " + authData.password.email + "! Your token expires " + (new Date(authData.expires * 1000));
+      $scope.loggedIn = true;
+
+      var tasksRef = ref.child(authData.uid);
+      
+      // until new security rules are placed, there is nothing preventing
+      // an user from overwriting another user's data or reading them.
+
+      var sync = $firebase(tasksRef);
+      $scope.tasks = sync.$asArray();
+
+      $scope.addTask = function(taskDone, taskDescription, taskCategory, taskDueDate) {
+        taskDescription = taskDescription || "My Task";
+        taskCategory = taskCategory || "~uncategorized";
+        taskDueDate = taskDueDate || "asap";
+        
+        $scope.tasks.$add({ done: taskDone,
+                            description: taskDescription,
+                            category: taskCategory,
+                            createDate: (new Date()).getTime(),
+                            dueDate: taskDueDate,
+                          });
+        $scope.taskDone = false;
+        $scope.taskDescription = "";
+        $scope.taskCategory = "";
+        $scope.taskDueDate = "";
+
+        $scope.$broadcast("newTaskAdded");
+      };
+    }
+
+    function clean() {
+      $scope.loginStatus = "Please log in";
+      $scope.tasks = [];
+      $scope.loggedIn = false;
+    }
+
+    $scope.logout = function() {
+      ref.unauth();
+      document.location.href = "login.html";
+    }
+    
+    var authData = ref.getAuth();
+    if (authData) {
+      init(authData);
+    } else {
+      clean();
+    }
+  }]);
+
+  app.directive("focusOn", function() {
+    return function(scope, elem, attr) {
+      scope.$on(attr.focusOn, function(e) {
+        elem[0].focus();
+      });
+    };
+  });
+})();
