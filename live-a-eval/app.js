1ed59f06a0b74249d4d9b27788000cd527fd03e7
Heitor Chang <heitorpontual@gmail.com>
2014-11-16 15:17:14 -0200

    definitions and commands

--- live-a-eval/app.js
+++ live-a-eval/app.js
@@ -3,14 +3,35 @@
   var editor = CodeMirror.fromTextArea(document.getElementById("demotext"), {
     lineNumbers: true,
     mode: "text/javascript",
-    matchBrackets: true
+    matchBrackets: true,
+    extraKeys: {
+      "Ctrl-Enter": function() { (1, eval)(editor.getValue()); $("#resulttext").val(getCommandsResult($("#commands").val())); }
+    }
   });
 
+  $("#commands").keydown(function (e) {
+    if ((e.keyCode == 10 || e.keyCode == 13) && e.ctrlKey) {
+      (1, eval)(editor.getValue());
+      $("#resulttext").val(getCommandsResult($("#commands").val()));
+    }
+  });                        
+
+  var getCommandsResult = function(commands) {
+    var result = "";
+    var commandsArray = commands.split('\n');
+    for (var i = 0; i < commandsArray.length; i++) {
+      result += (1, eval)(commandsArray[i]) + "\n";
+    }
+    return result;
+  };
+  
   // var ref = new Firebase("https:// ??? .firebaseio.com/liveAEval");
 
   app.controller('evalController', ['$scope', '$firebase', function($scope, $firebase) {
     $scope.myEval = function(code) {
-      $scope.result = eval(editor.getValue());
+      (1, eval)(editor.getValue());
+      $scope.result = getCommandsResult($scope.ngCommands);
+      
     };
   }]);
 })();
