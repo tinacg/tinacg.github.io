c63c2d0aca90ae6ee7c3c1b9f56cac0d2352725f
Heitor Chang <heitorpontual@gmail.com>
2014-11-16 22:15:40 -0200

    save and load

--- live-a-eval/app.js
+++ live-a-eval/app.js
@@ -25,7 +25,7 @@
     return result;
   };
   
-  // var ref = new Firebase("https:// ??? .firebaseio.com/liveAEval");
+  var ref = new Firebase("https://tinacg.firebaseio.com/live-a-eval");
 
   app.controller('evalController', ['$scope', '$firebase', function($scope, $firebase) {
     $scope.myEval = function(code) {
@@ -33,5 +33,100 @@
       $scope.result = getCommandsResult($scope.ngCommands);
       
     };
+
+    var savedDefsRef = null;
+    var savedDefsSync = null;
+    $scope.savedDefs = null;
+
+    $scope.currentEntry = "new";
+    
+    function setupSavedDefsRef(authData) {
+      savedDefsRef = ref.child(authData.uid).child("savedDefs");
+      savedDefsSync = $firebase(savedDefsRef);
+      $scope.savedDefs = savedDefsSync.$asArray();
+      // $scope.savedDefs.$add({ name: 'hello' });
+    }
+
+    $scope.addEntry = function(title) {
+      title = title || "Untitled";
+      defs = editor.getValue() || "// no defs";
+      commands = $scope.ngCommands || "";
+      $scope.savedDefs.$add({ title: title,
+                              defs: defs,
+                              commands: commands,
+                            })
+        .then(function() { $scope.infoMessage = "Saved " + title; });
+    };
+
+    $scope.loadEntry = function(id) {
+      if (id === 'new') {
+        editor.setValue("");
+        $scope.ngCommands = "";
+        $scope.result = "";
+        $scope.currentEntryTitle = "";
+      } else {
+        var entryRef = savedDefsRef.child(id);
+        var entrySync = $firebase(entryRef);
+        var entryObj = entrySync.$asObject();
+
+        entryObj.$loaded().then(function() {
+          editor.setValue(entryObj.defs);
+          $scope.ngCommands = entryObj.commands;
+          $scope.currentEntryTitle = entryObj.title;
+        });
+      }
+    };
+
+    $scope.saveCurrent = function(id) {
+      if (id === 'new') {
+        $scope.infoMessage = "Cannot save into New, use Add new";
+      } else {
+        var entryRef = savedDefsRef.child(id);
+        var entrySync = $firebase(entryRef);
+
+        entrySync.$set({
+          title: $scope.currentEntryTitle,
+          defs: editor.getValue(),
+          commands: $scope.ngCommands,
+        })
+          .then($scope.infoMessage = "Saved existing " + $scope.currentEntryTitle);
+      }
+    };
+
+    // USER MANAGEMENT    
+    function checkLogin() {
+      var authData = ref.getAuth();
+      if (authData) {
+        $scope.infoMessage = "Welcome back " + authData.password.email;
+        $scope.loggedIn = true;
+        setupSavedDefsRef(authData);
+      } else {
+        $scope.infoMessage = "Please log in.";
+        $scope.loggedIn = false;
+      }
+    }
+
+    $scope.logout = function() {
+      ref.unauth();
+      checkLogin();
+    }
+
+    $scope.tryLogin = function(email, pass) {
+      ref.authWithPassword({
+        email: email,
+        password: pass,
+      }, function(err, authData) {
+        if (err === null) {
+          checkLogin();
+          $scope.$apply();
+        } else {
+          $scope.infoMessage = err;
+          // clean refs and array
+          $scope.$apply();
+        }
+      }); //.then(function() { checkLogin(); });
+    };
+
+    checkLogin();
   }]);
 })();
