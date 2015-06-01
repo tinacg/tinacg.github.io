f647d8e2f68ea3f08c0bdcb4b9b3edd44b28bb2d
Heitor Chang <heitorpontual@gmail.com>
2015-06-01 16:11:11 -0300

    common lisp sharp-quote

--- taggednotes/commonlispnotes.js
+++ taggednotes/commonlispnotes.js
@@ -30,7 +30,7 @@ body: "<br>Anything not NIL is considered true.<br>NOTE: '() is the same as NIL<
 note4: { 
 id: "note4", 
 title: "Calling a function manually",
-tags: ['funcall'],
+tags: ['funcall', 'sharp-quote'],
 reference: "24 apr 2015, http://learnxinyminutes.com/docs/common-lisp/",
 body: "<br>(funcall #'+ 1 2 3)  ; 6<br><br>#' is read &quot;sharp-quote&quot;<br>" },
 
@@ -55,5 +55,12 @@ tags: ['secret', 'enlightenment', 'lists', 'functions', 'atom', 'nil', 'empty li
 reference: "29 apr 2015",
 body: "<br>Adding 1 + 1<br><br>The function call is a list<br><br>? (+ 1 1)<br>2<br><br>The list is a function call (calling cons multiple times)<br><br>? (cons (quote +) (cons 1 (cons 1 nil)))<br>(+ 1 1)<br><br>? (eval (cons (quote +) (cons 1 (cons 1 nil))))<br>2<br><br><br>? (cons 'list nil)<br>(list)<br><br>Is the empty list a list?<br>No, an empty list is the nil symbol, and a symbol is merely an atom.<br><br>http://stackoverflow.com/questions/16606172/is-an-empty-list-in-lisp-built-from-a-cons-cell<br><br>? (atom '())  ; because '() is the same as nil<br>T<br><br>? (atom nil)<br>T<br><br>? (atom (quote (1)))<br>NIL<br>" },
 
-numNotes: 8
+note8: { 
+id: "note8", 
+title: "Using sharp-quote",
+tags: ['functions', 'sharp-quote'],
+reference: "1 jun 2015, On Lisp p. 15",
+body: "<br>Sharp-quote, #' is used only for referring to the function named by a<br>symbol, usually those defined with defun. It expands to (function <expr>).<br>" },
+
+numNotes: 9
 };
