8f001d4ab3529c68fbe09f513c224ea62b8e43c1
Heitor Chang <heitorchang@gmail.com>
2015-08-11 22:44:50 -0300

    biwa wrappe

---
+++ biwa-wrapper/process.js
@@ -0,0 +1,22 @@
+var intp = new BiwaScheme.Interpreter();
+
+document.getElementById("evalSingle").addEventListener("click", function () {
+  intp.evaluate("(print " + document.getElementById("singleLineCode").value + ")");
+});
+
+document.getElementById("clearSingle").addEventListener("click", function() {
+  document.getElementById("singleLineCode").value = "";
+});
+
+document.getElementById("eval").addEventListener("click", function () {
+  intp.evaluate(document.getElementById("code").value);
+});
+
+document.getElementById("clearCode").addEventListener("click", function() {
+  document.getElementById("code").value = "";
+});
+
+document.getElementById("clearOutput").addEventListener("click", function() {
+  document.getElementById("bs-console").innerHTML = "";
+});
+
