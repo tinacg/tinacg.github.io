755fd2dee15aede1a982d35dc14b381e1bd28c53
Heitor Chang <heitorchang@gmail.com>
2015-08-22 21:02:50 -0300

    java primitives

--- taggednotes/javanotes.js
+++ taggednotes/javanotes.js
@@ -4,14 +4,28 @@ id: "note0",
 title: "Increasing orders of structure",
 tags: ['syntax'],
 reference: "7 apr 2015 Java nutshell 6th, p. 17",
-body: "characters<br>literal values, identifiers, tokens<br>data types<br>operators (groups individual tokens)<br>statements (groups expressions to form logical chunks)<br>methods (named collections of statements that can be invoked)<br>classes (collections of methods and fields)<br>packages (collections of related classes)<br>programs (interacting classes from one or more packages)<br>" },
+body: "<br>characters<br>literal values, identifiers, tokens<br>data types<br>operators (groups individual tokens)<br>statements (groups expressions to form logical chunks)<br>methods (named collections of statements that can be invoked)<br>classes (collections of methods and fields)<br>packages (collections of related classes)<br>programs (interacting classes from one or more packages)<br>" },
 
 note1: { 
 id: "note1", 
 title: "Comments",
 tags: ['comments'],
 reference: "7 apr 2015 Java nutshell 6th, p. 19",
-body: "//  single line<br><br>/*  */  multi-line<br><br>/**<br> * doc comment<br> */<br>" },
+body: "<br>//  single line<br><br>/*  */  multi-line<br><br>/**<br> * doc comment<br> */<br>" },
 
-numNotes: 2
+note2: { 
+id: "note2", 
+title: "The five reference types",
+tags: ['reference types'],
+reference: "22 aug 2015 Java nutshell 6th, p. 99",
+body: "<br>Classes<br>Interfaces<br>Arrays<br>Enumerated types (enums)<br>Annotation types (usually just called &quot;annotations&quot;)<br>" },
+
+note3: { 
+id: "note3", 
+title: "The eight primitive types",
+tags: ['primitives'],
+reference: "22 aug 2015 Java nutshell 6th, p. 22",
+body: "<br>boolean (true or false)<br>char (Unicode 16-bit character)<br><br>byte   8 bits signed integer<br>short  16 bits<br>int    32 bits<br>long   64 bits<br><br>float  32 bits<br>double 64 bits<br>" },
+
+numNotes: 4
 };
