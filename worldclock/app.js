3b0d474fc5cb166afe2f664d865e73b8a645daa5
Heitor Chang <heitorchang@gmail.com>
2015-11-28 15:57:39 -0200

    comment out hk

--- worldclock/app.js
+++ worldclock/app.js
@@ -1,7 +1,7 @@
 var localtime = document.getElementById("local");
 var chi = document.getElementById("chi");
 var tw = document.getElementById("tw");
-var hk = document.getElementById("hk");
+// var hk = document.getElementById("hk");
 
 moment.tz.load({
   zones: [],
@@ -14,4 +14,4 @@ moment.tz.names();
 localtime.innerHTML = moment().tz("America/Sao_Paulo").format("HH:mm");
 chi.innerHTML = moment().tz("America/Chicago").format("HH:mm");
 tw.innerHTML = moment().tz("Asia/Taipei").format("HH:mm");
-hk.innerHTML = moment().tz("Asia/Hong_Kong").format("HH:mm");
+// hk.innerHTML = moment().tz("Asia/Hong_Kong").format("HH:mm");
